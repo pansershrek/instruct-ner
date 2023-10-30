0a1d513b36ce766397ef2f3c76a26ba26d1599ae
Gleb Skiba <g.skiba@tinkoff.ru>
2023-10-30 21:00:03 +0300

    Update inference

--- instruction_ner/inference_instruct_llamacpp.py
+++ instruction_ner/inference_instruct_llamacpp.py
@@ -7,54 +7,92 @@ from transformers import GenerationConfig
 from llama_cpp import Llama
 from utils.rudrec.rudrec_reader import create_train_test_instruct_datasets
 from utils.nerel_bio.nerel_reader import create_instruct_dataset
+from utils.conll2003.conll_reader import create_instruct_dataset as create_instruct_dataset_conll
 from metric import extract_classes
+from transformers import (
+    AutoModelForCausalLM, AutoTokenizer,
+    DataCollatorForSeq2Seq, GenerationConfig,
+    DataCollatorForTokenClassification, EvalPrediction,
+    T5ForConditionalGeneration, Trainer, TrainerCallback,
+    TrainerControl, TrainerState, TrainingArguments
+)
+from train_utils import fix_model, fix_tokenizer, set_random_seed, SUPPORTED_DATASETS
+from peft import (
+    LoraConfig, PeftConfig, PeftModel, get_peft_model,
+    prepare_model_for_kbit_training
+)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset_name", default='rudrec', type=str, help='name of dataset')
+    parser.add_argument("--dataset_name", default='conll2003', type=str, help='name of dataset')
     parser.add_argument("--data_path", default='data/rudrec/rudrec_annotated.json', type=str, help='train file_path')
     parser.add_argument("--model_path", default='poteminr/llama2-rudrec', type=str, help='ggml model path')
-    parser.add_argument("--model_name", default='poteminr/llama2-rudrec', type=str, help='model name from hf')
+    parser.add_argument("--model_name", default="bigscience/bloomz-3b", type=str, help='model name from hf')
     parser.add_argument("--prediction_path", default='prediction.json', type=str, help='path for saving prediction')
     parser.add_argument("--max_instances", default=-1, type=int, help='max number of instruction')
     parser.add_argument("--max_new_tokens", default=512, type=int, help='max number of generated tokens')
     arguments = parser.parse_args()
 
-    model = Llama(
-        model_path=arguments.model_path,
-        n_gpu_layers = 35,
-        n_ctx=2048,
-        n_parts=1,
-        use_mmap=False,
+    model = AutoModelForCausalLM.from_pretrained(
+        arguments.model_name,
+        load_in_8bit=True,
+        device_map='cuda:0',
+        #n_gpu_layers = 35,
+        #n_ctx=2048,
+        #n_parts=1,
+        #use_mmap=False,
     )
-    generation_config = GenerationConfig.from_pretrained(arguments.model_name)
+    tokenizer = AutoTokenizer.from_pretrained(arguments.model_name)
+    model = fix_model(model, tokenizer, use_resize=False)
+    model = prepare_model_for_kbit_training(model)
+    model = PeftModel.from_pretrained(
+        model,
+        "/home/admin/instruct-ner/instruction_ner/models/checkpoint-219",
+    )
+
     max_new_tokens = arguments.max_new_tokens
-    
-    if arguments.dataset_name == 'rudrec': 
+
+    generation_config = GenerationConfig(
+        temperature=0.1,
+        top_p=0.75,
+        top_k=40,
+        num_beams=4,
+        early_stopping = True,
+        no_repeat_ngram_size = 3,
+        penalty_alpha = 0.6,
+        #epsilon_cutoff = 0.1,
+        length_penalty = -10,
+        eos_token_id=tokenizer.eos_token_id
+    )
+
+    if arguments.dataset_name == 'rudrec':
         from utils.rudrec.rudrec_utis import ENTITY_TYPES
         _, test_dataset = create_train_test_instruct_datasets(arguments.data_path)
         if arguments.max_instances != -1 and arguments.max_instances < len(test_dataset):
             test_dataset = test_dataset[:arguments.max_instances]
-    else:
+    elif arguments.dataset_name == 'nerel_bio':
         from utils.nerel_bio.nerel_bio_utils import ENTITY_TYPES
         test_path = os.path.join(arguments.data_path, 'test')
         test_dataset = create_instruct_dataset(test_path, max_instances=arguments.max_instances)
-    
+    else:
+        from utils.conll2003.conll_utils import ENTITY_TYPES
+        test_dataset = create_instruct_dataset_conll(
+            split='validation', max_instances=arguments.max_instances
+        )
+
     extracted_list = []
     target_list = []
     instruction_ids = []
     sources = []
-    
+
     for instruction in tqdm(test_dataset):
-        input_ids = model.tokenize(instruction['source'])
-        input_ids.append(model.token_eos())
+        #input_ids = model.tokenize(instruction['source'])
+        input_ids = tokenizer.encode(input_ids, skip_special_tokens=True)
+        input_ids.append(tokenizer.token_eos())
         generator = model.generate(
                 input_ids,
-                top_k=generation_config.top_k,
-                top_p=generation_config.top_p,
-                temp=generation_config.temperature,
-                repeat_penalty=generation_config.repetition_penalty,
+                generation_config=generation_config,
                 reset=True,
         )
 
@@ -63,14 +101,14 @@ if __name__ == "__main__":
             completion_tokens.append(token)
             if token == model.token_eos() or (max_new_tokens is not None and i >= max_new_tokens):
                 break
-            
+
         completion_tokens = model.detokenize(completion_tokens).decode("utf-8")
         extracted_list.append(extract_classes(completion_tokens), ENTITY_TYPES)
         instruction_ids.append(instruction['id'])
         target_list.append(instruction['raw_entities'])
-    
+
     pd.DataFrame({
-        'id': instruction_ids, 
+        'id': instruction_ids,
         'extracted': extracted_list,
         'target': target_list
     }).to_json(arguments.prediction_path)
