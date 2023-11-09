b1481be2d5e93bc5df9b34ee59654cf33ea67298
Gleb Skiba <g.skiba@tinkoff.ru>
2023-11-09 19:02:37 +0300

    Update evaluate

--- instruction_ner/inference_instruct.py
+++ instruction_ner/inference_instruct.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     parser.add_argument("--model_name", default='acrastt/Marx-3B-V2', type=str, help='model name from hf')
     parser.add_argument("--prediction_path", default='marx_prediction.json', type=str, help='path for saving prediction')
     parser.add_argument("--max_instances", default=-1, type=int, help='max number of instruction')
-    parser.add_argument("--batch_size", default=4, type=int, help='number of instructions in batch')
+    parser.add_argument("--batch_size", default=16, type=int, help='number of instructions in batch')
     parser.add_argument("--peft_config", default="/home/admin/instruct-ner/instruction_ner/marx_models/checkpoint-872", type=str, help="Path to peft config")
     arguments = parser.parse_args()
 
