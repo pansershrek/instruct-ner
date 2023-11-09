507ff549935563c0f91bf2841dd18320a8049494
Gleb Skiba <g.skiba@tinkoff.ru>
2023-11-09 12:59:17 +0300

    Add Marx-3B-V2 model

--- instruction_ner/inference_instruct.py
+++ instruction_ner/inference_instruct.py
@@ -18,42 +18,75 @@ def batch(iterable, n=4):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset_name", default='rudrec', type=str, help='name of dataset')
+    parser.add_argument("--dataset_name", default='conll2003', type=str, help='name of dataset')
     parser.add_argument("--data_path", default='data/rudrec/rudrec_annotated.json', type=str, help='train file_path')
-    parser.add_argument("--model_type", default='llama', type=str, help='model type')
-    parser.add_argument("--model_name", default='poteminr/llama2-rudrec', type=str, help='model name from hf')
-    parser.add_argument("--prediction_path", default='prediction.json', type=str, help='path for saving prediction')
+    parser.add_argument("--model_type", default='marx', type=str, help='model type')
+    parser.add_argument("--model_name", default='acrastt/Marx-3B-V2', type=str, help='model name from hf')
+    parser.add_argument("--prediction_path", default='marx_prediction.json', type=str, help='path for saving prediction')
     parser.add_argument("--max_instances", default=-1, type=int, help='max number of instruction')
     parser.add_argument("--batch_size", default=4, type=int, help='number of instructions in batch')
+    parser.add_argument("--peft_config", default="", type=str, help="Path to peft config")
     arguments = parser.parse_args()
 
-    assert arguments.dataset_name in SUPPORTED_DATASETS, f'expected dataset name from {SUPPORTED_DATASETS}'
+    # assert arguments.dataset_name in SUPPORTED_DATASETS, f'expected dataset name from {SUPPORTED_DATASETS}'
 
-    model_name = arguments.model_name
-    generation_config = GenerationConfig.from_pretrained(model_name)
-    
-    peft_config = PeftConfig.from_pretrained(arguments.model_name)
-    base_model_name = peft_config.base_model_name_or_path
-    
-    models = {'llama': AutoModelForCausalLM, 't5': T5ForConditionalGeneration}
-    
-    model = models[arguments.model_type].from_pretrained(
-        base_model_name,
+    # model_name = arguments.model_name
+    # generation_config = GenerationConfig.from_pretrained(model_name)
+
+    # peft_config = PeftConfig.from_pretrained(arguments.model_name)
+    # base_model_name = peft_config.base_model_name_or_path
+
+    # models = {'llama': AutoModelForCausalLM, 't5': T5ForConditionalGeneration}
+
+    # model = models[arguments.model_type].from_pretrained(
+    #     base_model_name,
+    #     load_in_8bit=True,
+    #     device_map='auto'
+    # )
+
+    # model = PeftModel.from_pretrained(model, model_name)
+    # tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    model = AutoModelForCausalLM.from_pretrained(
+        arguments.model_name,
         load_in_8bit=True,
-        device_map='auto'
+        device_map='cuda:0',
+        #n_gpu_layers = 35,
+        #n_ctx=2048,
+        #n_parts=1,
+        #use_mmap=False,
     )
-    
-    model = PeftModel.from_pretrained(model, model_name)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    
+    tokenizer = AutoTokenizer.from_pretrained(arguments.model_name)
+    model = fix_model(model, tokenizer, use_resize=False)
+    model = prepare_model_for_kbit_training(model)
+    model = PeftModel.from_pretrained(
+        model,
+        arguments.peft_config,
+    )
+    #model = model.merge_and_unload()
+    max_new_tokens = arguments.max_new_tokens
+
+    generation_config = GenerationConfig(
+        temperature=0.1,
+        top_p=0.75,
+        top_k=40,
+        num_beams=4,
+        early_stopping = True,
+        no_repeat_ngram_size = 3,
+        penalty_alpha = 0.6,
+        #epsilon_cutoff = 0.1,
+        length_penalty = -10,
+        eos_token_id=tokenizer.eos_token_id
+    )
+
     model.eval()
     model = torch.compile(model)
-    
-    if torch.cuda.device_count() > 1:
-        model.is_parallelizable = True
-        model.model_parallel = True
-    
-    if arguments.dataset_name == 'rudrec': 
+
+    #if torch.cuda.device_count() > 1:
+    #    model.is_parallelizable = True
+    #    model.model_parallel = True
+
+    if arguments.dataset_name == 'rudrec':
         from utils.rudrec.rudrec_reader import create_train_test_instruct_datasets
         from utils.rudrec.rudrec_utis import ENTITY_TYPES
 
@@ -63,29 +96,29 @@ if __name__ == "__main__":
     elif arguments.dataset_name == 'nerel_bio':
         from utils.nerel_bio.nerel_reader import create_instruct_dataset
         from utils.nerel_bio.nerel_bio_utils import ENTITY_TYPES
-        
+
         test_path = os.path.join(arguments.data_path, 'test')
         test_dataset = create_instruct_dataset(test_path, max_instances=arguments.max_instances)
     else:
         from utils.conll2003.conll_reader import create_instruct_dataset
         from utils.conll2003.conll_utils import ENTITY_TYPES
-        
+
         test_dataset = create_instruct_dataset(split='test', max_instances=arguments.max_instances)
-    
+
     extracted_list = []
     target_list = []
     instruction_ids = []
     sources = []
-    
+
     for instruction in tqdm(test_dataset):
         target_list.append(instruction['raw_entities'])
         instruction_ids.append(instruction['id'])
         sources.append(instruction['source'])
-       
+
     target_list = list(batch(target_list, n=arguments.batch_size))
-    instruction_ids = list(batch(instruction_ids, n=arguments.batch_size))    
+    instruction_ids = list(batch(instruction_ids, n=arguments.batch_size))
     sources = list(batch(sources, n=arguments.batch_size))
-    
+
     for source in tqdm(sources):
         input_ids = tokenizer(source, return_tensors="pt", padding=True)["input_ids"].cuda()
         with torch.no_grad():
@@ -99,9 +132,9 @@ if __name__ == "__main__":
         for s in generation_output.sequences:
             string_output = tokenizer.decode(s, skip_special_tokens=True)
             extracted_list.append(extract_classes(string_output, ENTITY_TYPES))
-    
+
     pd.DataFrame({
-        'id': np.concatenate(instruction_ids), 
+        'id': np.concatenate(instruction_ids),
         'extracted': extracted_list,
         'target': np.concatenate(target_list)
     }).to_json(arguments.prediction_path)
